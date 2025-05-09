1f703529a3294548c123a9b6235aac7becad3f6f
Jhon Fredy <150628209+Delta-Jhon-Fredy@users.noreply.github.com>
2025-05-09 12:56:29 -0500

    Generar eleccion usuario
    Generar asociacion de eleccion por metodo de entrada

--- script.js
+++ script.js
@@ -20,3 +20,14 @@ function getComputerChoice () {
   return randomNumber(1 , 3)
 
 }
+
+function getHumanChoice (){
+  let humanChoice = prompt("Choose between: 'rock', 'paper' y 'scissors'");
+  return humanChoice;
+}
+    
+  let userScore = 0;
+      let computerScore = 0;
+
+ console.log("Game: Rock, paper and scissors")
+      console.log("-°--°--°--°--°-")
